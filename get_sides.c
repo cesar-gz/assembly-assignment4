d7d673282b3ef877480297ac8cbcd21dda3fa928
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-11-03 22:00:53 -0700

    blur

--- get_sides.c
+++ get_sides.c
@@ -1,1 +1,13 @@
+// Name: Cesar Gutierrez
+// CWID: 886646686
+// Email: cesarg7@csu.fullerton.edu
+
+#include <stdio.h>
+
+void get_sides(double* side1, double* side2, double* side3){
+    double d;
+    scanf("%lf", d);
+    printf("%d", d);
+
+}
 
