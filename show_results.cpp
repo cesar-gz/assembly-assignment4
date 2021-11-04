8617967f348cc7b2dc733a6e153a8389634c4851
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-11-03 21:40:35 -0700

    added placeholder code

--- show_results.cpp
+++ show_results.cpp
@@ -1,1 +1,13 @@
+// Name: Cesar Gutierrez
+// CWID: 886646686
+// Email: cesarg7@csu.fullerton.edu
 
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void show_results(float a, float b, float c, float d){
+
+    //I'm assuming you need to pass four floats in order to print them out 
+    cout << "The area of a triangle with sides " << a << ", " << b << ", and" << c << " is " << d << "square units." << endl;
+}
