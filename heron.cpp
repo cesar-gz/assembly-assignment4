85b25f4233f09038ed2ee3249f2db9cc6796bdec
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-11-03 21:29:25 -0700

    added code

--- heron.cpp
+++ heron.cpp
@@ -1,1 +1,18 @@
+// Name: Cesar Gutierrez
+// CWID: 886646686
+// Email: cesarg7@csu.fullerton.edu
 
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+extern "C" long triangle();
+
+int main(){
+	
+    long returnValue;
+	cout << endl;
+	cout << "Welcome to CPSC 240 Assignment 4 brought to you by Cesar Gutierrez." << endl;
+    returnValue = triangle();
+	return 0;
+}
